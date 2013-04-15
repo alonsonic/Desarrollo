bd7e59bb5ca02c47b2b31fd33ad1f29cfa168a6f
Alonso Araujo <alonsonic@gmail.com>
2013-04-15 18:29:39 -0430

    Primer commit

---
+++ AeiCliente/AeiCliente/GUI/ItemProducto.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using AeiCliente.ServiceReference1;
+
+ 
+// La plantilla de elemento Control de usuario está documentada en http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AeiCliente
+{
+    public sealed partial class ItemProducto : UserControl
+    {
+
+        public ItemProducto()
+        {
+            this.setImagenProducto("ms-appx:/App_Data/item.png.png");
+            this.InitializeComponent();
+
+            
+            
+            
+        }
+
+        public void setImagenProducto(String imageSource)
+        {
+            BitmapImage newImage = new BitmapImage();
+            newImage.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+            newImage.UriSource = new Uri(imageSource);
+            this.imagenProducto.Source = newImage;
+            
+        }
+
+        private void ButtonDetail_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+    }
+}
---
+++ AeiCliente/AeiCliente/Usuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeiCliente
+{
+    public static class Usuario
+    {
+
+        static string pasaporte = null;
+        static string correo = null;
+        static DateTime fechaNac;
+        static string direccion = null;
+
+        static public string Pasaporte
+        {
+            get { return pasaporte; }
+            set { pasaporte = value; }
+        }
+        
+
+        static public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+        
+
+        static public DateTime FechaNac
+        {
+            get { return fechaNac; }
+            set { fechaNac = value; }
+        }
+
+        static public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        static public bool isConectado(){
+
+            if (pasaporte == null)
+                return false;
+            else
+                return true;
+        }
+        
+
+    }
+}
