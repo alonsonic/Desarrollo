74b5b3c95ebebad2fe44d7894a6a97198bcdb320
Isaac Arismendi <arimendi6702@hotmail.com>
2013-04-27 03:15:23 -0430

    error tag casi listo

---
+++ WebCams/WebCams/Xmlcs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebCams
+{
+    public partial class Xmlcs : Form
+    {
+        public Xmlcs()
+        {
+            InitializeComponent();
+        }
+        public Xmlcs(string xml)
+        {
+            InitializeComponent();
+            labelXml.Text = xml;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
