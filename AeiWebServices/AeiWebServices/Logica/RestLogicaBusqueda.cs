1aba4d1b55d084489740615bf0e81cfb366a0aa9
Isaac Arismendi <arimendi6702@hotmail.com>
2013-06-21 22:37:45 -0430

    casi listo el consumir el servicio rest
    solo falta adaptar las cosas para q funcionen con el servicio del grupo
    "x"

---
+++ AeiWebServices/AeiWebServices/Logica/RestLogicaBusqueda.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Xml;
+
+namespace AeiWebServices.Logica
+{
+    public class RestLogicaBusqueda
+    {
+
+        private XmlDocument recibirXml(string ruta)
+        {
+            HttpWebRequest req = WebRequest.Create(ruta) as HttpWebRequest;
+            string result = null;
+            using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(resp.GetResponseStream());
+                result = reader.ReadToEnd();
+            }
+
+            XmlDocument xmlRest = new XmlDocument();
+            xmlRest.LoadXml(result);
+            return xmlRest;
+        }
+
+        public List<Producto> listaProductos(string ipServidorRest, string parametrosBusquedad)
+        {
+            List<Producto> listaProductos = new List<Producto>();
+            string ruta = "http://" + ipServidorRest + ":3306/GrailsApplication3/productos/rest/"+ parametrosBusquedad;
+            XmlDocument xmlRest = recibirXml(ruta);
+ 
+            /**** LOS PARAMETROS DEL XML HAY Q ARREGLARLOS PARA EL XML DEL EQUIPO Q NOS TOCO ******/
+            XmlNodeList Productos = xmlRest.GetElementsByTagName("productoes");
+            XmlNodeList lista = ((XmlElement)Productos[0]).GetElementsByTagName("producto");
+            foreach (XmlElement nodo in lista)
+            {
+                XmlNodeList lista1 = nodo.GetElementsByTagName("nombreProducto");
+            }
+
+            return listaProductos;  
+
+        }
+
+    }
+}
