26639782463f2543012cd4c5a1144f5a7c91edbf
Isaac Arismendi <arimendi6702@hotmail.com>
2013-06-23 17:42:15 -0430

    detalle producto casi listo
    solo falta cambiar el titulo y poner la imagen

--- AeiMobile/AeiMobile/ArticuloPivotPage.xaml.cs
+++ AeiMobile/AeiMobile/ArticuloPivotPage.xaml.cs
@@ -10,14 +10,44 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using AeiMobile.ServicioAEI;
 
 namespace AeiMobile
 {
     public partial class ArticuloPivotPage : PhoneApplicationPage
     {
-        public ArticuloPivotPage()
+        private Producto producto;
+
+        public ArticuloPivotPage(Producto producto)
         {
             InitializeComponent();
+            this.producto = producto;
+            cargarInformacionProducto();
+        }
+
+        private void cargarInformacionProducto()
+        {
+            this.textPrecio.Text = producto.Precio.ToString() + " Bs";
+            this.textCantidad.Text = "Cantidad: " + producto.Cantidad.ToString();
+            this.textDescripcion.Text = producto.Descripcion;
+            cargarListaCalifiacion();
+        }
+
+        private void cargarListaCalifiacion()
+        {
+            if(producto.Calificaciones.Count > 0)
+            {
+                for (int i = 0; i < producto.Calificaciones.Count; i++)
+                {
+                    this.listCalificacion.Items.Add(producto.Calificaciones[i].Usuario.Nombre + " " + producto.Calificaciones[i].Usuario.Apellido + ". \n Fecha: "+producto.Calificaciones[i].Fecha.ToString("dd-MM-yyyy")+
+                            ".  Puntaje: " +producto.Calificaciones[i].Puntaje+ " estrella(s). \n Comentario: " + producto.Calificaciones[i].Comentario);
+                }
+            }
+            else
+            {
+               this.listCalificacion.Items.Add("\n \n Aun no tenemos calificaciones para este producto.");
+            }
+            
         }
     }
 }
