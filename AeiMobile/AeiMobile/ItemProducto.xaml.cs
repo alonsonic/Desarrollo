97798d525f0078a48fff37f5eb62de57f7e938ab
Alonso Araujo <alonsonic@gmail.com>
2013-06-27 21:44:51 -0430

    Agregadas carritoPage, perfilPage, items

---
+++ AeiMobile/AeiMobile/ItemProducto.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using AeiMobile.ServicioAEI;
+
+namespace AeiMobile
+{
+    public partial class ItemProducto : UserControl
+    {
+        private Producto producto;
+
+
+        public ItemProducto(Producto producto)
+        {
+            InitializeComponent();
+            boton.Content = producto.Nombre;
+            this.producto = producto;
+        }
+
+        private void boton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Navegar hacia pantalla detalle del producto
+        }
+    }
+}
---
+++ AeiMobile/AeiMobile/ItemProductoCarrito.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using AeiMobile.ServicioAEI;
+
+namespace AeiMobile
+{
+    public partial class ItemProductoCarrito : UserControl
+    {
+        private Producto producto;
+
+        public ItemProductoCarrito(Producto producto)
+        {
+            InitializeComponent();
+            boton.Content = producto.Nombre;
+            this.producto = producto;
+        }
+
+        private void botonBorrar_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Borrar este producto del carrito del usuario
+        }
+
+        private void boton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Navegar hacia el detalle del producto
+        }
+    }
+}
